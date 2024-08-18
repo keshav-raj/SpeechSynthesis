e07d3d69d47d7560fb00a5f576637f96c7c0e2c3
keshav raj <keshavkumark7@gmail.com>
2024-08-18 09:03:54 +0530

    Speechify

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,112 +1,202 @@
-import Image from "next/image";
+"use client";
+
+import React from "react";
 
 export default function Home() {
+  const [volume, setVolume] = React.useState(0.5);
+  const [rate, setRate] = React.useState(1);
+  const [voices, setVoices] = React.useState<Array<SpeechSynthesisVoice>>([]);
+  const [preText, setPreText] = React.useState("");
+  const [readingText, setReadingText] = React.useState("");
+  const [remainingText, setRemainingText] = React.useState("");
+  const synth: SpeechSynthesis | null =
+    typeof window !== "undefined" ? window.speechSynthesis : null;
+  let speechUtterance = null;
+  React.useEffect(() => {
+    const populateVoiceList = () => {
+      if (typeof synth === "undefined") {
+        return;
+      }
+      const voices = synth?.getVoices();
+      setVoices(voices || []);
+    };
+    populateVoiceList();
+
+    // in Google Chrome the voices are not ready on page load
+    if (synth && "onvoiceschanged" in synth) {
+      synth.onvoiceschanged = populateVoiceList;
+    }
+    return () => {
+      synth?.cancel();
+    };
+  }, [synth]);
+  const onSpeakInit = () => {
+    synth?.cancel();
+    const textToSpeech = (
+      document.getElementById("textToSpeech") as HTMLTextAreaElement
+    )?.value;
+
+    if (!textToSpeech) {
+      alert("Please give some thoughts to read");
+      return;
+    }
+    speechUtterance = new SpeechSynthesisUtterance(textToSpeech);
+
+    const selectedVoice = (
+      document.getElementById("voice") as HTMLSelectElement
+    ).value;
+    speechUtterance.voice = voices[Number(selectedVoice || 0)];
+
+    speechUtterance.volume = volume;
+    speechUtterance.rate = rate;
+
+    const selectedPitch = (
+      document.getElementById("pitch") as HTMLSelectElement
+    ).value;
+    speechUtterance.pitch = Number(selectedPitch || 1);
+    // Split the text into words
+    speechUtterance.onboundary = (event) => {
+      let nextSpaceIndex = event.charIndex + event.charLength;
+      let preText = textToSpeech.substring(0, event.charIndex);
+      let readingText = textToSpeech.substring(event.charIndex, nextSpaceIndex);
+      let remainingText = textToSpeech.substring(nextSpaceIndex);
+
+      setPreText(preText);
+      setReadingText(readingText);
+      setRemainingText(remainingText);
+    };
+    const element = document.getElementById("parser");
+    element?.scrollIntoView({ behavior: "smooth" });
+    synth?.speak(speechUtterance);
+  };
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main className='flex min-h-screen flex-col items-center justify-between p-2'>
+      <div className='min-h-screen w-full grid grid-cols-1 lg:grid-cols-2 gap-4'>
+        <div className=''>
+          <div className='rounded-md h-full w-full flex-row justify-center p-6'>
+            <p className='font-bold'>{`Let's Read Together`}</p>
+            <div className='pt-6'>
+              <label
+                htmlFor='message'
+                className='block mb-2 text-sm font-medium text-gray-900 dark:text-white'
+              >
+                Your message
+              </label>
+              <textarea
+                id='textToSpeech'
+                rows={4}
+                className='block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'
+                placeholder='Write your thoughts here...'
+              ></textarea>
+            </div>
+            <div className='pt-6'>
+              <label
+                htmlFor='volume-range'
+                className='block mb-2 text-sm font-medium text-gray-900 dark:text-white'
+              >
+                Volume {parseInt(`${volume * 100}`)} %
+              </label>
+              <input
+                id='volume-range'
+                type='range'
+                defaultValue='0.5'
+                onChange={(e) => {
+                  setVolume(Number(e.target.value));
+                }}
+                min={0}
+                max={1}
+                step={0.05}
+                className='w-full h-2 bg-gray-200 rounded-lg appearance-none cursor-pointer dark:bg-gray-700'
+              />
+            </div>
+            <div className='pt-6'>
+              <label
+                htmlFor='rate'
+                className='block mb-2 text-sm font-medium text-gray-900 dark:text-white'
+              >
+                Rate {rate}
+              </label>
+              <input
+                id='rate'
+                type='range'
+                min={0.5}
+                max={2}
+                defaultValue={1}
+                step={0.1}
+                onChange={(e) => {
+                  setRate(Number(e.target.value));
+                }}
+                className='w-full h-2 bg-gray-200 rounded-lg appearance-none cursor-pointer dark:bg-gray-700'
+              />
+            </div>
+            <div className='pt-6'>
+              <label
+                htmlFor='pitch'
+                className='block mb-2 text-sm font-medium text-gray-900 dark:text-white'
+              >
+                Select Pitch
+              </label>
+              <select
+                id='pitch'
+                className='bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'
+              >
+                <option value='0'>Low</option>
+                <option selected value={1}>
+                  Medium
+                </option>
+                <option value='2'>High</option>
+              </select>
+            </div>
+
+            <div className='pt-6'>
+              <label
+                htmlFor='voice'
+                className='block mb-2 text-sm font-medium text-gray-900 dark:text-white'
+              >
+                Select Voice
+              </label>
+              <select
+                id='voice'
+                className='bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'
+              >
+                {voices.map((voice, index) => {
+                  return (
+                    <option key={`${index}_voice`} value={index}>
+                      {voice.name}
+                    </option>
+                  );
+                })}
+              </select>
+            </div>
+            <div className='pt-6'>
+              <button
+                onClick={onSpeakInit}
+                type='button'
+                className='focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800'
+              >
+                {`Let's Read`}
+              </button>
+              <button
+                onClick={() => {
+                  synth?.cancel();
+                }}
+                type='button'
+                className='focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-800'
+              >
+                stop
+              </button>
+            </div>
+          </div>
         </div>
-      </div>
-
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+        <div id='parser' className='w-full h-full'>
+          <div className='h-full w-[70%] rounded-md m-12 bg-white'>
+            <p className='p-6 text-black'>
+              <span>{preText}</span>
+              <span className='bg-[#7F1B10] text-white'>{readingText}</span>
+              <span>{remainingText}</span>
+            </p>
+          </div>
+        </div>
       </div>
     </main>
   );
